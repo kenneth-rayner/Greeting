51561c17aec2340b6a269a47c8312135243f6878
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-01-22 11:35:45 +0000

    standard import

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -1,7 +1,14 @@
 package app
 
+import scala.io.StdIn
+
 object GreeterApplication extends App{
 
-  println("Hello")
+  def greet(name: String) : Unit = println("Hello " + name)
 
+  val name = StdIn.readLine("What is your name?")
+
+  greet(name)
+
+  
 }
