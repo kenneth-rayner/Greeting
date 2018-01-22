354786857cc1757375dcb3d525338e226fd20774
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-01-22 12:10:40 +0000

    if else statement

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -4,11 +4,18 @@ import scala.io.StdIn
 
 object GreeterApplication extends App{
 
-  def greet(name: String) : Unit = println("Hello " + name)
+  def greet(name: String) : Unit = {
+    if (name == "adam") {
+      println("You don't get a hello!")
+    }
+    else {
+      println ("Hello " + name)
+    }
+  }
 
   val name = StdIn.readLine("What is your name?")
 
   greet(name)
 
-  
+
 }
