d57cc8d8e88136fe92fefa4e619eaeb5fee03e86
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-01-24 14:34:37 +0000

    add animals info

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -1,7 +1,40 @@
 package app
 
+
 import scala.io.StdIn
 
+
+abstract class BankAccount(accountNumber: String, val balance: Double){
+  def withdraw(amount: Double): BankAccount
+  def deposit(amount: Double): BankAccount
+}
+
+class SavingsAccount(accountNumber:String,balance: Double) extends BankAccount(accountNumber, balance) {
+
+  override def withdraw(amount: Double): BankAccount = {
+    if ((balance - amount) < 0) {
+      println(s"You have insufficient funds")
+      this
+    } else {
+      val deducted = balance - amount
+      println(s"Balance after deductions:$deducted")
+      new SavingsAccount(accountNumber, deducted)
+    }
+  }
+
+  override def deposit(amount: Double): BankAccount = new SavingsAccount(accountNumber, balance + amount)
+}
+
+  final class CashISASavingsAccount(accountNumber: String, balance :Double) extends BankAccount(accountNumber, balance) {
+    override def withdraw(amount: Double): BankAccount = {
+      println(s"You can't withdraw yet, your money is locked in for 3 years!!!!")
+      this
+    }
+
+    override def deposit(amount: Double): BankAccount = {
+      new CashISASavingsAccount(accountNumber, balance + amount)
+    }
+  }
 class Person(name: String, age : Int){
 
   private val years : String = if(age>1)"years" else " year"
@@ -23,12 +56,25 @@ object Prompt{
 
 object GreeterApplication extends App{
 
+  val savingsAccount = new SavingsAccount("12345",100.00)
+  val savingsPlus100 = savingsAccount.deposit(50.00)
+
+  val CashISASavingsAccount = new CashISASavingsAccount("12345",100.00)
+  val CashISASavingsAccountplus100= CashISASavingsAccount.deposit(50.00)
+  val CashISASavingsAccountminus100= CashISASavingsAccount.withdraw(50.00)
+
+  println(savingsAccount.balance)
+  println(savingsPlus100.balance)
+  println(CashISASavingsAccount.balance)
+  println(CashISASavingsAccountplus100.balance)
+  println(CashISASavingsAccountminus100.balance)
+
+
 
   val name = Prompt.ask("What is your name?")
   val age : String = Prompt.ask("How old are you?")
   val p = new Person(name, age.toInt)
   println(p.speak())
-
 
 
 }
---
+++ src/main/scala/app/MyAnimals.scala
@@ -0,0 +1,51 @@
+package app
+
+
+ abstract class Animal{
+  def speak ():String
+}
+
+class Cat extends Animal {
+  override def speak(): String =  "Meow"
+}
+class Dog extends Animal{
+  override def speak(): String =  "Woof"
+}
+
+object MyAnimals extends scala.App {
+
+  val jess : Animal = new Cat()
+  val lassie : Animal = new Dog()
+  val fred: Meowth = new Meowth(40, false)
+  val bill: BorderTerrier = new BorderTerrier()
+  val jack: Wolf = new Wolf()
+  val kev: PersianCat = new PersianCat()
+
+
+  println(jess.speak())
+  println(lassie.speak())
+  println(fred.speak())
+  println(bill.hunt())
+  println(jack.speak())
+  println (kev.speak())
+}
+
+class Wolf extends Dog{
+  override def speak(): String =  "Howl!!"
+
+}
+
+class BorderTerrier extends Dog{
+  def hunt(): String = "Border Terrier is hunting"
+}
+
+class PersianCat extends Cat {
+  }
+
+class Meowth(coins: Int, TeamRocket: Boolean) extends Cat {
+ private val rocket : String = if (TeamRocket){
+   "I am in team Rocket"
+ } else {"I am not in team Rocket"}
+  override def speak(): String = s"That's right, I have $coins coins,$rocket "
+
+}
