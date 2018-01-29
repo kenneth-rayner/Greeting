c38173e715e7e53086ab1a9e89c85bff1ed18b9e
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-01-29 15:16:09 +0000

    refactor and introduce lists

--- src/main/scala/app/Controllers/GreeterApplication.scala
+++ src/main/scala/app/Controllers/GreeterApplication.scala
@@ -1,6 +1,8 @@
 package app.Controllers
 
 import app.Models.{CashISASavingsAccount, Person, SavingsAccount}
+import app.Views.Prompt
+
 
 object GreeterApplication extends App{
 
@@ -21,20 +23,21 @@ object GreeterApplication extends App{
   val cashIsa = new CashISASavingsAccount("12345",0.00)
   val isaDeposited = cashIsa.deposit(1000.00)
   val withdrawFromIsa = isaDeposited.withdraw(200.00)
-  val personWithCashIsa = new Person("Loyal customer",56,withdrawFromIsa)
+  val personWithCashIsa = new Person("Loyal customer",56,List(withdrawFromIsa))
   val superAccount =new CashISASavingsAccount("1223468",0.00,depositThreshold =800.00).deposit(1000.00).withdraw(00.00)
-  val superPersonWithIsa = new Person("Fred", 56, superAccount)
+  val superPersonWithIsa = new Person("Fred", 56, List(superAccount))
 
   println(superPersonWithIsa.speak())
+  val nameSet = Set ("Adam","Mark","Dave","Adam")
+  println(nameSet)
 
-
-//  val name = Prompt.ask("What is your name?")
-  //val age : String = Prompt.ask("How old are you?")
- // val p = new Person(name, age.toInt)
+  val name = Prompt.ask("What is your name?")
+  val age : String = Prompt.ask("How old are you?")
+  val p = new Person(name, age.toInt,Nil)
  // val child = new Person("David")
   //val p2  = new Person ("Ken","Rayner")
 
-//  println(p.speak())
+   println(p.speak())
  // println(child.speak())
   // println(p2.speak())
 
--- src/main/scala/app/Models/Person.scala
+++ src/main/scala/app/Models/Person.scala
@@ -1,31 +1,33 @@
 package app.Models
 
-class Person(name: String, age : Int,private val bankAccount: BankAccount){
+class Person(name: String, age : Int,private val accounts:Seq[BankAccount] =Nil){
+
+  private val excludedNames : List[String] =List("Adam", "Daniel", "John","Michael")
 
 def this(name: String, age: Int)= this(
  name = name,
  age = age,
- bankAccount = new SavingsAccount("123",0.00))
+  accounts= List(new SavingsAccount("123",0.00)))
 
 def this (name: String) = this(
  name=name,
  age = 0,
- bankAccount = new SavingsAccount("1234",0.00)
-)
+  accounts= List(new SavingsAccount("1234",0.00)))
+
 def this(firstName:String,
       lastName:String) = this(
 name = s"$firstName $lastName",
 age =0,
-bankAccount = new SavingsAccount("12345",0.00)
-)
+  accounts= List(new SavingsAccount("12345",0.00)))
+
 private val years : String = if(age==1)"year" else " years"
 
 def speak() : String = {
- if(name == "adam") {
+ if(excludedNames.contains(name)) {
    "You don't get a hello"
  }
  else {
-    s"Hello $name, you are $age$years old.\nYour bank details are as follows:\n${bankAccount} "
+    s"Hello $name, you are $age$years old.\nYour bank details are as follows:\n${accounts} "
  }
 }
 }
